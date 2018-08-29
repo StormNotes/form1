97c80e25635f25b8f3af5ab48f26640562cd331a
Leonardo Diaz <stormnotesnotes@gmail.com>
2018-08-29 18:01:04 +0200

    update one

---
+++ main.js
@@ -0,0 +1,8 @@
+$('.toggle').click(function(){
+    $('.forms').animate({
+        height: "toggle",
+        'padding-top': 'toggle',
+        'padding-bottom': 'toggle',
+        opacity:'toggle'
+    }, "slow")
+});
