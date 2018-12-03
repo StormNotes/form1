33681d0723ef4250587191295eff6b7cc8eb0616
Leonardo Diaz <stormnotesnotes@gmail.com>
2018-12-03 00:57:13 -1200

    update y tal

--- main.js
+++ main.js
@@ -1,39 +1,57 @@
-$('.toggle').click(function(){
+$('.toggle').click(function () {
     $('.forms').animate({
         height: "toggle",
         'padding-top': 'toggle',
         'padding-bottom': 'toggle',
-        opacity:'toggle'
+        opacity: 'toggle'
     }, "slow")
 });
 
-function ValidateForm(){
-    var username= document.getElementById("username");
-    var phoneNum= document.getElementById("phoneNum");
-    var password= document.getElementById("password");
-    var confirm_password= document.getElementById("confirm_password");
-    if(username.value==""){
-    username.className="wrong-input";
-    username.nextElementSibling.innerHTML="Username can't be blank";        
+function ValidateForm() {
+    var username = document.getElementById("username");
+    var phoneNum = document.getElementById("phoneNum");
+    var password = document.getElementById("password");
+    var confirm_password = document.getElementById("confirm_password");
+    removeMessage();
+    var valid = true;
+    if (username.value === "") {
+        username.className = "wrong-input";
+        username.nextElementSibling.innerHTML = "Username can't be blank";
+        valid = false;
     }
-    else if(username.value.length < 3){
-    username.className="wrong-input";
-    username.nextElementSibling.innerHTML="Username can't be less than 3 letters";        
+    else if (username.value.length < 3) {
+        username.className = "wrong-input";
+        username.nextElementSibling.innerHTML = "Username can't be less than 3 letters";
+        valid = false;
     }
-    if(phoneNum.value.length<10){
-     phoneNum.className="wrong-input";
-     phoneNum.nextElementSibling.innerHTML="Contact number cannot be less than 10";
-     
+
+    if (phoneNum.value.length < 7) {
+        phoneNum.className = "wrong-input";
+        phoneNum.nextElementSibling.innerHTML = "Contact number cannot be less than 7";
+        valid = false;
     }
-    if(password.value.length<3){
-     password.className="wrong-input";
-     password.nextElementSibling.innerHTML="Password cannot be less than 6";
-     
+    if (password.value.length < 6) {
+        password.className = "wrong-input";
+        password.nextElementSibling.innerHTML = "Password cannot be less than 6";
+        valid = false;
     }
-    else if(password.value!=confirm_password.value){
-     confirm_password.className="wrong-input";
-     confirm_password.nextElementSibling.innerHTML="Password does not match";
-     
+    if (password.value !== confirm_password.value) {
+        confirm_password.className = "wrong-input";
+        confirm_password.nextElementSibling.innerHTML = "Password does not match";
+        valid = false;
     }
-    return false;
- }
+
+    return valid;
+
+}
+function removeMessage() {
+    const errorinput = document.querySelectorAll(".wrong-input");
+    [].forEach.call(errorinput, function (el) {
+        el.classList.remove("wrong-input");
+    });
+
+    const errorpara = document.querySelectorAll(".error");
+    [].forEach.call(errorpara, function (el) {
+        el.innerHTML = "";
+    });
+}
